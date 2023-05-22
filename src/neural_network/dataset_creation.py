2851417d488d19e6236f66730e06de4b3644d86d
DmitryjVeselyj <veselyj.dv@edu.spbstu.ru>
2023-05-23 01:11:40 +0300

    Тмп+ изменения

--- src/neural_network/dataset_creation.py
+++ src/neural_network/dataset_creation.py
@@ -1,12 +1,26 @@
 import numpy as np
+import pandas as pd
+from ta.trend import sma_indicator, ema_indicator
+from ta.utils import dropna
+from ta.momentum import rsi
+from ta.volatility import BollingerBands
+from ta.volume import on_balance_volume, money_flow_index, acc_dist_index, volume_price_trend, force_index, chaikin_money_flow
 
 
-def create_dataset(df, time_offset):
+def create_dataset(df, time_offset, volume=[]):
     x = []
     y = []
     for i in range(time_offset, df.shape[0]):
-        x.append(df[i-time_offset:i, 0])
+        tmp_df = df[i-time_offset:i, 0]
+        # rs = rsi(pd.Series(tmp_df), window=28)
+        sma_12 = sma_indicator(pd.Series(tmp_df))
+        sma_55 = sma_indicator(pd.Series(tmp_df), window=55)
+        obv = force_index(pd.Series(tmp_df), pd.Series(volume[i - time_offset:i, 0]))
+        x.append(tmp_df)
+        # x.append(np.array([sma_12[len(sma_12) - 1] ,sma_55[len(sma_55) - 1], obv[len(obv) - 1]]))
+        # x.append(np.array([ta.sma(tmp_df, length = 10), ta.ema(tmp_df, length = 10)]))
         y.append(df[i, 0])
     x = np.array(x)
     y = np.array(y)
     return x,y
+
--- src/neural_network/model.py
+++ src/neural_network/model.py
@@ -1,17 +1,19 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
+from keras.layers import LSTM, Dense, Dropout, SimpleRNN
+
 
 def create_model(inp_shape):
-    model = Sequential()
-    model.add(LSTM(units=96, return_sequences=True, input_shape=(inp_shape, 1)))
-    model.add(Dropout(0.5))
-    model.add(LSTM(units=96,return_sequences=True))
-    model.add(Dropout(0.5))
-    model.add(LSTM(units=96,return_sequences=True))
-    model.add(Dropout(0.5))
-    model.add(LSTM(units=96))
-    model.add(Dropout(0.5))
+    model = Sequential(name="LSTM_1_model")
+    model.add(LSTM(units=48,return_sequences=True, input_shape=inp_shape))
+    model.add(Dropout(0.2))
+    # model.add(LSTM(units=66,return_sequences=True))
+    # model.add(Dropout(0.2))
+    model.add(LSTM(units=48, return_sequences=True))
+    # # model.add(Dropout(0.2))
+    model.add(LSTM(units=48))
+    # model.add(Dropout(0.2))
     model.add(Dense(units=1))
-    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    
     return model
